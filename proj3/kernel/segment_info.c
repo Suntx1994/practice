3ec63dd115772218721f354a77257ffd616d1174
Allen Hsu <yhsu018@ucr.edu>
2018-03-02 18:10:43 -0800

    modification of segment_info

--- proj3/kernel/segment_info.c
+++ proj3/kernel/segment_info.c
@@ -5,8 +5,10 @@ asmlinkage int show_segment_info(int pid) {
     if (task) {
         struct mm_struct *this_mm = task->mm;
         if (this_mm) {
-            printk(KERN_INFO "[Memory segment addresses of process %d]:\n%08lx - %08lx: code segment\n%08lx - %08lx: data segment\n%08lx - %08lx: heap segment\n",
-                   pid, this_mm->start_code, this_mm->end_code, this_mm->start_data, this_mm->end_data, this_mm->start_brk, this_mm->brk);
+            printk(KERN_INFO "[Memory segment addresses of process %d]", pid);
+            printk(KERN_INFO "%08lx - %08lx: code segment", this_mm->start_code, this_mm->end_code);
+            printk(KERN_INFO "%08lx - %08lx: data segment", this_mm->start_data, this_mm->end_data);
+            printk(KERN_INFO "%08lx - %08lx: heap segment", this_mm->start_brk, this_mm->brk);
             return 0;
         }
     }
